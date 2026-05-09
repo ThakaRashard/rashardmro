f92f126e0b284cabf02cb063605d34c95abc97ef
Rashard Iman Kelly @nasa-jpl @stateofcalifornia <rashardiman@proton.me>
2026-05-09 13:13:22 -0700

    Update types.sh

--- assets/scripts/types.sh
+++ assets/scripts/types.sh
@@ -1,7 +1,7 @@
 #################⁸#####
 echo "CURRENT DNS MAP @la-county-isd @whitehouse"
 date
-echo " #NEWS ra5hard.github.io  && cnn.com/2001/TECH/industry/09/11/akamai.founder/index.html"
+echo " #NEWS ra5hard.github.io @nasa-jpl  && cnn.com/2001/TECH/industry/09/11/akamai.founder/index.html"
 echo "https://archive.org/details/losangelesenormo0000weav #NEWS ra5hard.github.io  && cnn.com/2001/TECH/industry/09/11/akamai.founder/index.html"
 uname -a
 ifconfig 
